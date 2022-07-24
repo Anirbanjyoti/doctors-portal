598cd42b0a6d89f23cdd615f32b269e6c31a3306
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-07-24 16:42:40 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <button class="btn btn-primary">Button</button>
+    <div>
+    <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
     themes: [
       {
         doctorstheme: {
-          primary: "#a991f7",
-          secondary: "#f6d860",
-          accent: "#37cdbe",
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
