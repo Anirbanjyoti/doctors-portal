4dce8eff12be35b84a1dd74ca8f70bd32aa5f006
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-05 22:42:06 +0530

    Limit Dashboard features based on admin level access

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,24 +1,41 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
+import auth from "../../firebase.init";
+import useAdmin from "../../hooks/useAdmin";
 
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  // calling useAdmin hooke for restricted admin panel
+  const {admin} = useAdmin(user);
   return (
     <div className="drawer drawer-mobile">
       <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        <h2 className="text-5xl font-bold text-purple-500">Welcome to Your Dashboard</h2>
+        <h2 className="text-5xl font-bold text-purple-500">
+          Welcome to Your Dashboard
+        </h2>
         <Outlet></Outlet>
         {/* <!-- Page content here --> */}
-        
       </div>
       <div className="drawer-side">
         <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
         <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
           {/* <!-- Sidebar content here --> */}
-          <li><Link to="/dashboard">My Appointment</Link></li>
-          <li><Link to="/dashboard/reviews">My Reviews</Link></li>
-          <li><Link to="/dashboard/history">History</Link></li>
-          <li><Link to="/dashboard/users">ALl Users</Link></li>
+          <li>
+            <Link to="/dashboard">My Appointment</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/reviews">My Reviews</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/history">History</Link>
+          </li>
+          {admin && (
+            <li>
+              <Link to="/dashboard/users">ALl Users</Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
