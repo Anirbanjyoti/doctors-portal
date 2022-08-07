4b8e37735d08c14053cf6d80e4f4f7512ee39069
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-07 11:39:58 +0530

    deploy firebase and heroku

--- src/Pages/Dashboard/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor.js
@@ -2,25 +2,79 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useQuery } from "react-query";
 import Loading from "../Shared/Loading";
+import { toast } from "react-toastify";
 
 const AddDoctor = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset
   } = useForm();
   // Load mongodb api data by useQuery
   const { data: services, isLoading } = useQuery(["services"], () =>
     fetch("http://localhost:5000/service").then((res) => res.json())
   );
+
   if (isLoading) {
     return <Loading></Loading>;
   }
  
+  const imageStorageKey='f3a6088d5c548faa1303685172a82865';
   // Onsubmit
-  const onSubmit = async (data) => {
-    console.log(data);
-  };
+  /**
+   * 3 ways to store images
+   * 1. Third party storage //Free open public storage is ok for Practice project 
+   * 2. Your own storage in your own server (file system)
+   * 3. Database: Mongodb 
+   * 
+   * YUP: to validate file: Search: Yup file validation for react hook form
+  */
+   const onSubmit = async data => {
+    const image = data.image[0];
+    const formData = new FormData();
+    formData.append('image', image);
+    const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    .then(res=>res.json())
+    .then(result =>{
+        if(result.success){
+            const img = result.data.url;
+            const doctor = {
+                name: data.name,
+                email: data.email,
+                specialty: data.specialty,
+                img: img
+            }
+            // send to your database 
+            fetch('http://localhost:5000/doctor', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify(doctor)
+            })
+            .then(res =>res.json())
+            .then(inserted =>{
+                if(inserted.insertedId){
+                    toast.success('Doctor added successfully')
+                    reset();
+                }
+                else{
+                    toast.error('Failed to add the doctor');
+                }
+            })
+
+        }
+        
+    })
+}
+
+
   return (
     <div className="w-1/2 mx-auto">
       <h1 className="text-2xl">Add a Doctor</h1>
