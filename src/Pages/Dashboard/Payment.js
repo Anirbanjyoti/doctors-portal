f8e7f49071d4161153f569b36bd9f5bc5f49a12a
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-08 19:10:38 +0530

    create payment route and get appointment by id and load appointment

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React from "react";
 import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+// Stripe Promise
+const stripePromise = loadStripe('pk_test_51LUSGESJ50KZEocag9uS9z8ltJ58ZFsKmm4YA3FefdsNfbvHjqlIkiHXgZpi46JGlAb48ezyHHdnUSjCCJZ9OLKZ00rYXL9Xoj');
 
 const Payment = () => {
-    const {id} = useParams()
-    return (
-        <div>
-            <h1 className='text-2xl text-purple-500'>Please Pay For : {id}</h1>
+  const { id } = useParams();
+  const url = `http://localhost:5000/booking/${id}`;
+  const { data: appointment, isLoading } = useQuery(["booking", id], () =>
+    fetch(url, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if(isLoading){
+    return <Loading></Loading>
+  }
+
+  return (
+    <div>
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <p className="text-success font-bold">Hello, {appointment.patientName}</p>
+                    <h2 class="card-title">Please Pay for {appointment.treatment}</h2>
+                    <p>Your Appointment: <span className='text-orange-700'>{appointment.date}</span> at {appointment.slot}</p>
+                    <p>Please pay: ${appointment.price}</p>
+                </div>
+            </div>
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm appointment={appointment} />
+                    </Elements>
+                </div>
+            </div>
         </div>
-    );
+  );
 };
 
 export default Payment;
